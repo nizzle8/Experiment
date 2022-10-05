b4c366cee78f8b22fedc004af682a56d79fdecd6
Mathew Edwards <mathew.edwards@hotmail.co.uk>
2022-10-05 17:26:36 +0200

    added movement to navbar. Created boxes for main page text

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navOpen = document.querySelector(".fa-bars");
 const navbar = document.getElementById("navbar");
 const navClose = document.querySelector(".fa-xmark");
+const pages = document.querySelectorAll(".page");
+const boxes = document.querySelectorAll("box");
 
 navOpen.addEventListener("click", () => {
     navbar.classList.toggle("active");
@@ -8,5 +10,11 @@ navOpen.addEventListener("click", () => {
 
 navClose.addEventListener("click", () => {
     navbar.classList.toggle("active");
+})
+
+pages.forEach(page => {
+    page.addEventListener("click", () => {
+        page.classList.contains(1)
+    })
 })
 
